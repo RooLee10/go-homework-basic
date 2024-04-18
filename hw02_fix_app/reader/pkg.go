79b0d79a89e31cbd6c4d28f52bfa457765dda97a
Vadim Li <mail@mail.ru>
2024-04-18 03:16:03 +0300

    fix ci lint

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -9,7 +9,7 @@ import (
 	"github.com/RooLee10/go-homework-basic/hw02_fix_app/types"
 )
 
-func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
+func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
