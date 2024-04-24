09954f0bebada4958b55fddc25c4ecf57b98316e
Vadim Li <mail@mail.ru>
2024-04-24 22:30:44 +0300

    hw03 done

--- hw03_chessboard/go.mod
+++ hw03_chessboard/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw03_chessboard
+module github.com/RooLee10/go-homework-basic/hw03_chessboard
 
 go 1.20
--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	length := 8
+	height := 8
+
+	for i := 1; i <= height; i++ {
+		fmt.Print("|")
+		for j := 1; j <= length; j++ {
+			switch {
+			case i%2 == 1 && j%2 == 1:
+				fmt.Print(" ")
+			case i%2 == 1 && j%2 == 0:
+				fmt.Print("#")
+			case i%2 == 0 && j%2 == 1:
+				fmt.Print("#")
+			case i%2 == 0 && j%2 == 0:
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("|")
+	}
 }
